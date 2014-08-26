2782d31cdf3f7cd78cacf349a54b23fc29f2c8c7
EasyMK <xuexiwenjianTT@163.com>
2014-08-26 17:28:50 +0800

    8.26
    Nav complete

--- daohang/js/index.js
+++ daohang/js/index.js
@@ -53,4 +53,97 @@ $(function(){
 		$("#LM2").stop().animate({"top": "0"},2000);
 		$("#LM3").stop().animate({"top": "665"},2000);
 	});
+
+
+
+
+
+
+
+
+	$(".YS").hover(function(){
+		var currNum = $(this).attr("num");
+		showPeople(currNum);
+	},function(){
+		var currNum = $(this).attr("num");
+
+		hidePeople(currNum);
+	});
+
+
+
+
+
+	
+
+
+	$(".pic").hover(function(){
+		m();
+	},function(){
+		m2();
+	});
+
+	// $(".Next").hover(function(){
+	// 	m();
+	// },function(){
+	// 	m2();
+	// }); 
+
+	// $(".hidemore").hover(function(){
+	// 	m3();
+	// },function(){
+	// 	m3();
+	// }); 
+
+
+
+
+
+
+
+
 });
+
+function m(){
+		var aaa = event.clientX;
+		var aaaa = aaa - 100;
+		var bbb = event.clientY;
+		var bbbb = bbb + 15;
+		// alert(aaa+""+bbb);
+		$(".hidemore").css({
+			"top":bbbb,
+			"left":aaaa
+		});
+		$(".hidemore").stop().animate({"opacity": "100"},5000);
+	}
+
+function m2(){
+		// var aaa = event.clientX;
+		// var bbb = event.clientY;
+		// // alert(aaa+""+bbb);
+		$(".hidemore").css({
+			"top":"0",
+			"left":"0"
+		});
+		$(".hidemore").stop().animate({"opacity": "0"},0);
+	}
+
+// function m3(){
+// 		var aaa = event.clientX;
+// 		var aaaa = aaa -50;
+// 		var bbb = event.clientY;
+// 		// alert(aaa+""+bbb);
+// 		$(".hidemore").css({
+// 			"top":bbb,
+// 			"left":aaaa
+// 		});
+// 		$(".hidemore").stop().animate({"opacity": "100"},0);
+// 	}	
+
+
+// function showPeople(currNum){
+// 	$(".people"+currNum).stop().animate({"opacity": "100"},5000);
+// }
+// function hidePeople(currNum){
+// 	$(".people"+currNum).stop().animate({"opacity": "0"},1000);
+// }
